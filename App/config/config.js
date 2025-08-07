ed546a26495945908adb4bcad698a6efa1109391
G48R1 <s270082@studenti.polito.it>
2025-08-08 00:07:56 +0200

    eventi show fix

--- App/config/config.js
+++ App/config/config.js
@@ -4,11 +4,13 @@ const CANTI_DIR = AppRepo + "/Libretto/canti";
 const dirCantoExample = "Esempio"
 const SCHEMA_DIR = AppRepo + "/validation/schemas";
 const INDICE_CANTI_DIR = AppRepo + "/Libretto/indice-canti";
+const INDICE_EVENTI_DIR = AppRepo + "/Eventi/indice-eventi";
 const INDICE_CANTI_DOMENICA_DIR = AppRepo + "/Domenica/indice-canti-domenica";
 const CANTO_TEMPLATE_DIR = AppRepo + "/Libretto/canto-template";
+const EVENTO_TEMPLATE_DIR = AppRepo + "/Eventi/evento-template";
 const IMGS_LOGO_DIR = AppRepo + "/src/imgs";
 const FONTS_DIR = AppRepo + "/src/fonts";
 
 export {
-    CANTI_DIR, dirCantoExample, SCHEMA_DIR, INDICE_CANTI_DIR, INDICE_CANTI_DOMENICA_DIR, CANTO_TEMPLATE_DIR, IMGS_LOGO_DIR, FONTS_DIR, AppRepo
+    CANTI_DIR, dirCantoExample, SCHEMA_DIR, INDICE_CANTI_DIR, INDICE_EVENTI_DIR, INDICE_CANTI_DOMENICA_DIR, CANTO_TEMPLATE_DIR, EVENTO_TEMPLATE_DIR, IMGS_LOGO_DIR, FONTS_DIR, AppRepo
 }
